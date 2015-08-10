a4e35bdca7c36ea9446bb8c721a60fd2d42fb9b8
Dylan Graham <dylan.graham@gmail.com>
2015-08-10 10:36:44 +1000

    DB seeds. Controllers

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -14,3 +14,14 @@
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::resource('projects', 'ProjectsController');
+Route::resource('projects.tasks', 'TasksController');
+//Route::resource('tasks', 'TasksController');
+
+Route::bind('tasks', function($value, $route) {
+    return App\Task::whereSlug($value)->first();
+});
+Route::bind('projects', function($value, $route) {
+    return App\Project::whereSlug($value)->first();
+});
