33017876b0a05b261a49374b1f8c56a93435743b
Dylan Graham <dylan.graham@gmail.com>
2015-08-10 16:45:02 +1000

    Empty Partials

--- resources/views/projects/show.blade.php
+++ resources/views/projects/show.blade.php
@@ -7,18 +7,15 @@
         Your project has no tasks.
     @else
         <ul>
-            @foreach( $project->tasks as $task )
-                <li>
-                    {!! Form::open(array('class' => 'form-inline', 'method' => 'DELETE', 'route' => array('projects.tasks.destroy', $project->slug, $task->slug))) !!}
-                        <a href="{{ route('projects.tasks.show', [$project->slug, $task->slug]) }}">{{ $task->name }}</a>
-                        (
-                            {!! link_to_route('projects.tasks.edit', 'Edit', array($project->slug, $task->slug), array('class' => 'btn btn-info')) !!},
- 
-                            {!! Form::submit('Delete', array('class' => 'btn btn-danger')) !!}
-                        )
-                    {!! Form::close() !!}
-                </li>
-            @endforeach
+        @foreach( $project->tasks as $task )
+        <li>
+            {!! Form::open(array('class' => 'form-inline', 'method' => 'DELETE', 'route' => array('projects.tasks.destroy', $project->slug, $task->slug))) !!}
+            <a href="{{ route('projects.tasks.show', [$project->slug, $task->slug]) }}">{{ $task->name }}</a>
+            {!! link_to_route('projects.tasks.edit', 'Edit', array($project->slug, $task->slug), array('class' => 'btn btn-info')) !!}
+            {!! Form::submit('Delete', array('class' => 'btn btn-danger')) !!}
+            {!! Form::close() !!}
+        </li>
+        @endforeach
         </ul>
     @endif
 
@@ -26,5 +23,4 @@
         {!! link_to_route('projects.index', 'Back to Projects') !!} |
         {!! link_to_route('projects.tasks.create', 'Create Task', $project->slug) !!}
     </p>
-
 @endsection
